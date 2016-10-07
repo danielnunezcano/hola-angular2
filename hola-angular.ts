5047197c276c2fe2ccee7870405911cc5946e50f
Daniel Núñez <danielnunezcano@gmail.com>
2016-10-07 16:49:07 +0200

    version inicial del proyecto

---
+++ hola-angular.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+@Component({
+	selector: 'hola-angular',
+	template: '<h1> {{saludo}} </h1>'
+})
+class HolaAngularComponent {
+	saludo: string;
+	constructor(){
+		this.saludo = '¡Hola Angular 2!';
+	}
+}
+
+bootstrap(HolaAngularComponent); // Componente es bootstrapped!
